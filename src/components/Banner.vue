ea23c6486bb0122713dbdfeaa597cb4975952421
taylorcgaines <taylorcgaines@gmail.com>
2021-11-09 16:38:56 -0700

    feat: top half styling done.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -6,9 +6,10 @@
     <div class="header__copy">Since 1859, Gearbox Machine Corporation has specialized in the manufacture of precision-machined custom gears in a variety of sizes and types. Ask us about custom gears.</div>
     <a href="#" class="button"><span>Learn More</span></a>
     <div class="header__video-lightbox">
-      <div class="teaser-img"></div>
+      <div class="teaser-img"><img src="https://www.fillmurray.com/267/173
+" alt=""></div>
       <div class="teaser-content">
-        <a href="#">Play Video</a>
+        <a href="#"><img src="../assets/icons/play_button.svg" alt="play" />Play Video</a>
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
       </div>
     </div>
